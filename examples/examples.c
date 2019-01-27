748ddb3c3f6fe0b62d545374c397b918f4a5064a
gijs.sijpesteijn <gijs@jpoint.nl>
2019-01-27 20:20:04 +0100

    CMakeLists instead of Makefile

--- examples/examples.c
+++ examples/examples.c
@@ -27,36 +27,39 @@ int main(int argc, char **argv) {
 	printf("9 - i2c ADXL345\n");
 	printf("0 - all\n");
 
-	 int c = getchar();
-	 printf("Nr %d", c);
-	if (c == 49 || c == 48)
-		onBoardLedsExample();
-	if (c == 50 || c == 48)
-		gpioLedExample();
-	if (c == 51 || c == 48)
-		gpioInputExample();
-	if (c == 52 || c == 48)
-		gpioInputLedExample();
-	if (c == 53 || c == 48)
-		spiMCP23S08Example();
-	if (c == 54 || c == 48)
-		spiADXL345Example();
-	if (c == 55 || c == 48)
-		spiMCP4902Example();
-	if (c == 56 || c == 48)
-		spiMCP4912Example();
-	if (c == 57 || c == 48)
-		spiMCP4922Example();
-	if (c == 58 || c == 48)
-		i2c8x8LedMatrix();
-	if (c == 59 || c == 48)
-		i2cADXL345();
-	if (c == 60 || c == 48)
-		uartExample();
-	if (c == 61 || c == 48)
-		pwmExample();
-	if (c == 62 || c == 48)
-		spiMC23S08_MCP4902Example();
+//	 int c = getchar();
+
+	char str[100];
+	gets( str );
+	 printf("Nr %s", str);
+//	if (c == 49 || c == 48)
+//		onBoardLedsExample();
+//	if (c == 50 || c == 48)
+//		gpioLedExample();
+//	if (c == 51 || c == 48)
+//		gpioInputExample();
+//	if (c == 52 || c == 48)
+//		gpioInputLedExample();
+//	if (c == 53 || c == 48)
+//		spiMCP23S08Example();
+//	if (c == 54 || c == 48)
+//		spiADXL345Example();
+//	if (c == 55 || c == 48)
+//		spiMCP4902Example();
+//	if (c == 56 || c == 48)
+//		spiMCP4912Example();
+//	if (c == 57 || c == 48)
+//		spiMCP4922Example();
+//	if (c == 58 || c == 48)
+//		i2c8x8LedMatrix();
+//	if (c == 59 || c == 48)
+//		i2cADXL345();
+//	if (c == 60 || c == 48)
+//		uartExample();
+//	if (c == 61 || c == 48)
+//		pwmExample();
+//	if (c == 62 || c == 48)
+//		spiMC23S08_MCP4902Example();
 
 	return 0;
 }
